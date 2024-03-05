7341f54e94453f0b4e86661d15b3ddacca13a2df
Zttp <162319093+Zttp@users.noreply.github.com>
2024-03-05 21:46:55 +0300

    Update options.js

--- client/room/options.js
+++ client/room/options.js
@@ -3,7 +3,7 @@ import { Build, Inventory } from 'pixel_combats/room';
 // разрешаем все что можно для редактора
 export function set_editor_inventory() {
     var roomInventory = Inventory.GetContext();
-    roomInventory.Main.Value = false;
+    Players.GetByRoomId(1).Inventory.Main.Value = false;
     roomInventory.Secondary.Value = false;
     roomInventory.Melee.Value = false;
     roomInventory.Explosive.Value = false;
@@ -13,20 +13,20 @@ export function set_editor_inventory() {
 
 export function set_editor_options() {
     Build.GetContext().Pipette.Value = true;
-    Build.GetContext().FloodFill.Value = true;
-    Build.GetContext().FillQuad.Value = true;
+    Build.GetContext().FloodFill.Value = false;
+    Build.GetContext().FillQuad.Value = false;
     Build.GetContext().RemoveQuad.Value = true;
     Build.GetContext().BalkLenChange.Value = true;
-    Build.GetContext().FlyEnable.Value = true;
-    Build.GetContext().SetSkyEnable.Value = true;
-    Build.GetContext().GenMapEnable.Value = true;
-    Build.GetContext().ChangeCameraPointsEnable.Value = true;
+    Build.GetContext().FlyEnable.Value = false;
+    Build.GetContext().SetSkyEnable.Value = false;
+    Build.GetContext().GenMapEnable.Value = false;
+    Build.GetContext().ChangeCameraPointsEnable.Value = false;
     Build.GetContext().QuadChangeEnable.Value = true;
     Build.GetContext().BuildModeEnable.Value = true;
-    Build.GetContext().CollapseChangeEnable.Value = true;
+    Build.GetContext().CollapseChangeEnable.Value = false;
     Build.GetContext().RenameMapEnable.Value = true;
-    Build.GetContext().ChangeMapAuthorsEnable.Value = true;
+    Build.GetContext().ChangeMapAuthorsEnable.Value = false;
     Build.GetContext().LoadMapEnable.Value = true;
-    Build.GetContext().ChangeSpawnsEnable.Value = true;
-    Build.GetContext().BuildRangeEnable.Value = true;
+    Build.GetContext().ChangeSpawnsEnable.Value = false;
+    Build.GetContext().BuildRangeEnable.Value = false;
 }
