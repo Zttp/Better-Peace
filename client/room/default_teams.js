aaf561be8971140724457667b505c796f5a0680a
Zttp <162319093+Zttp@users.noreply.github.com>
2024-03-05 18:33:05 +0300

    Update default_teams.js

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -3,13 +3,13 @@ import { Teams } from 'pixel_combats/room';
 
 export const RED_TEAM_NAME = "Red";
 export const BLUE_TEAM_NAME = "Blue";
-export const RED_TEAM_DISPLAY_NAME = "<b>Красные</b>";
-export const BLUE_TEAM_DISPLAY_NAME = "<b>Синие</b>";
+export const RED_TEAM_DISPLAY_NAME = "<i><b>Игроки</b></i>";
+export const BLUE_TEAM_DISPLAY_NAME = "<i><b>Игроки</b></i>";
 export const BLUE_TEAM_SPAWN_POINTS_GROUP = 1;
 export const RED_TEAM_SPAWN_POINTS_GROUP = 2;
 
 export function create_team_red() {
-    Teams.Add(RED_TEAM_NAME, RED_TEAM_DISPLAY_NAME, new Color(1, 0, 0, 0));
+    Teams.Add(RED_TEAM_NAME, RED_TEAM_DISPLAY_NAME, new Color(190, 171, 188));
     Teams.Get(RED_TEAM_NAME).Spawns.SpawnPointsGroups.Add(RED_TEAM_SPAWN_POINTS_GROUP);
     return Teams.Get(RED_TEAM_NAME);
 }
