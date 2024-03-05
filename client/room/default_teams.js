89ebe59b43fca9bf1e1cb9559962111fd48c8a46
Zttp <162319093+Zttp@users.noreply.github.com>
2024-03-05 18:42:51 +0300

    Update default_teams.js

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -9,13 +9,13 @@ export const BLUE_TEAM_SPAWN_POINTS_GROUP = 1;
 export const RED_TEAM_SPAWN_POINTS_GROUP = 2;
 
 export function create_team_red() {
-    Teams.Add(RED_TEAM_NAME, RED_TEAM_DISPLAY_NAME, new Color(190, 171, 188));
+    Teams.Add(RED_TEAM_NAME, RED_TEAM_DISPLAY_NAME, new Color(190, 171, 188, 0));
     Teams.Get(RED_TEAM_NAME).Spawns.SpawnPointsGroups.Add(RED_TEAM_SPAWN_POINTS_GROUP);
     return Teams.Get(RED_TEAM_NAME);
 }
 
 export function create_team_blue() {
-    Teams.Add(BLUE_TEAM_NAME, BLUE_TEAM_DISPLAY_NAME, new Color(190, 171, 188));
+    Teams.Add(BLUE_TEAM_NAME, BLUE_TEAM_DISPLAY_NAME, new Color(190, 171, 188, 0));
     Teams.Get(BLUE_TEAM_NAME).Spawns.SpawnPointsGroups.Add(BLUE_TEAM_SPAWN_POINTS_GROUP);
     return Teams.Get(BLUE_TEAM_NAME);
 }
