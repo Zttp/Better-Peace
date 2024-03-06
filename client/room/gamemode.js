3d2af7ce43b72cb4efb76e79fbf0d535a4aa64f7
Zttp <162319093+Zttp@users.noreply.github.com>
2024-03-06 15:15:44 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -41,9 +41,8 @@ API.LeaderBoard.PlayerLeaderBoardValues = [
     }
 ];
 // statuses
-p.Properties.Get("stafus").Value ="status" 
-    if (p.id == "D411BD94CAE31F89") {p.Properties.Get("statuk").Value ="status"});
-
+p.Properties.Get("status").Value ="status" 
+    if (p.id == "D411BD94CAE31F89") {p.Properties.Get("status").Value ="status"}
 // разрешаем вход в команды по запросу
 Teams.OnRequestJoinTeam.add_Event(function (player, team) { team.Add(player); });
 // спавн по входу в команду
