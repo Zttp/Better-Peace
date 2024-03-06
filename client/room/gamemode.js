6fe5bfacef9eaff07bee4cc0cde5898ef1d2e6e4
Zttp <162319093+Zttp@users.noreply.github.com>
2024-03-06 20:50:18 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -26,7 +26,7 @@ export function apply_room_options() {
     buildContext.RemoveQuad.Value = gameModeParameters.GetBool("RemoveQuad");
     buildContext.FlyEnable.Value = gameModeParameters.GetBool("Fly");
 
-    contextedProperties.GetContext().SkinType.Value = 1 = gameModeParameters.GetBool("Zombie");
+    contextedProperties.GetContext().SkinType.Value = 2 = gameModeParameters.GetBool("Zombie");
     
     // прочие опции
     room.Damage.GetContext().DamageOut.Value = gameModeParameters.GetBool("Damage");
