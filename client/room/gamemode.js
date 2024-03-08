c7246a4dd5a8a2e3feb6fbd012255f18e13bd84d
Zttp <162319093+Zttp@users.noreply.github.com>
2024-03-08 08:23:44 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -42,7 +42,7 @@ Damage.GetContext().DamageOut.Value = true;
 Properties.GetContext().GameModeName.Value = "Better-Peace";
 contextedProperties.GetContext().SkinType.Value = 2;
 contextedProperties.GetContext().MaxHp.Value = 505;
-room.p.Properties.Get('status').Value = 'admin';
+room.player.Properties.Get('status').Value = 'admin';
 // создаем команды
 var red = GameMode.Parameters.GetBool("RedTeam");
 var blue = GameMode.Parameters.GetBool("BlueTeam");
@@ -54,7 +54,7 @@ LeaderBoard.PlayerLeaderBoardValues = [
 	new DisplayValueHeader("Kills", "<color=red>Убийства</a>", "<color=red>Убийства</a>"),
 	new DisplayValueHeader("Deaths", "<color=orange>Смерти</a>", "<color=orange>Смерти</a>"),
 	new DisplayValueHeader("Spawns", "<color=yellow>Спавны</a>", "<color=yellow>Спавны</a>"),
-	new DisplayValueHeader("status", "<color=lime>qupe</a>", "<color=green>qupe</a>")
+	new DisplayValueHeader("Status", "<color=lime>qupe</a>", "<color=green>qupe</a>")
 ];
 
 // разрешаем вход в команды по запросу
