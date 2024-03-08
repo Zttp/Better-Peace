3f05373a9a31a519962a5ae5597460d6bf859ab9
Zttp <162319093+Zttp@users.noreply.github.com>
2024-03-08 08:59:25 +0300

    Update options.js

--- client/room/options.js
+++ client/room/options.js
@@ -2,25 +2,6 @@ import { Build, Inventory } from 'pixel_combats/room';
 import * as room from 'pixel_combats/room';
 import * as teams from './default_teams.js';
 
-// разрешает все что можно для строительства
-function set_inventory() {
-    const context = room.Inventory.GetContext();
-    context.Main.Value = false;
-    context.Secondary.Value = false;
-    context.Melee.Value = true;
-    context.Explosive.Value = false;
-    context.Build.Value = true;
-    context.BuildInfinity.Value = true;
-}
-
-// задает в контекст инвентаря пустой инвентарь
-function set_empty_inventory(inventory) {
-    inventory.Main.Value = false;
-    inventory.Secondary.Value = false;
-    inventory.Melee.Value = false;
-    inventory.Explosive.Value = false;
-    inventory.Build.Value = false;
-
 export function set_editor_options() {
     Build.GetContext().Pipette.Value = true;
     Build.GetContext().FloodFill.Value = false;
